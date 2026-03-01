20488087351e76369695b3e26fc5c2ebe4f7aa33
Arjuna Carly <arjuna.carly1304@gmail.com>
2026-03-01 23:29:39 +0700

    add Controller (jobsheet 2)

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', function () {
-    return 'Gerbang';
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
 Route::get('/hello', function () {
     return 'Hello World';
-});
-Route::get('/world', function () {
-    return 'world';
 }); 
+
 Route::get('/salam', function () {
-    return 'Selamat Datang';
+    return 'Hello World';
 });
-Route::get('/about', function () {
-    return 'Nama : Arjuna Carly Dhymas Mahendra <br> NIM : 2341720156  Kelas : TI-2B';
-});
+
 Route::get('/user/{name?}', function ($name='John') {
  return 'Nama saya '.$name;
 });
+
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-Route::get('articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
 });
