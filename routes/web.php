8b073cf9071ffe293bc1c144201fa183449a6d57
Arjuna Carly <arjuna.carly1304@gmail.com>
2026-03-01 20:14:21 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Gerbang';
+});
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'world';
+}); 
+Route::get('/salam', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'Nama : Arjuna Carly Dhymas Mahendra <br> NIM : 2341720156  Kelas : TI-2B';
+});
+Route::get('/user/{name?}', function ($name='John') {
+ return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
